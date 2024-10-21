87abcd7940f4bfe3350099b9260cacf1652b18f0
Diego Chan <xdiego.chanx@gmail.com>
2024-10-21 16:37:27 -0500

    Update Header.jsx
    Header.jsx is now responsive

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,21 +1,45 @@
+import { useState, useEffect } from "react";
+
 export default function Header() {
+    const [navVisible, setNavVisible] = useState(window.innerWidth >= 1024);
+
+    useEffect(() => {
+        window.addEventListener("resize", () => {
+            setNavVisible(window.innerWidth >= 1024);
+        });
+    }, []);
+
     return (
-        <header id="website-header" className="bg-white sticky top-0 z-50 py-4 border-b border-neutral-400">
-            <div id="header-content" className="mx-12 flex items-center justify-between">
-                <a href="/" id="header-title" className="flex items-center gap-6">
-                    <i className="bi bi-brightness-high-fill flex-center text-3xl"></i>
-                    <h1 className="font-semibold text-2xl">Voyage</h1>
-                </a>
-                <div id="header-navigation" className="flex items-center gap-12">
-                    <nav id="header-navbar" className="flex gap-6">
-                        <a href="/" className="px-2 py-1 rounded hover:bg-neutral-200 active:bg-neutral-300 duration-150">Inicio</a>
-                        <a href="/commerce" className="px-2 py-1 rounded hover:bg-neutral-200 active:bg-neutral-300 duration-150">Comercio</a>
-                        <a href="/about-us" className="px-2 py-1 rounded hover:bg-neutral-200 active:bg-neutral-300 duration-150">Acerca de nosotros</a>
-                    </nav>
-                    <div id="signup-container">
-                        <a href="/signup" className="px-4 py-2 rounded bg-voyage-400 hover:bg-voyage-500 active:bg-voyage-600 text-white duration-150">Regístrate</a>
+        <header id="website-header" className="bg-white sticky top-0 z-10 py-6 border-b border-neutral-400">
+            <div id="header-content" className="relative mx-4 md:mx-8 lg:mx-12 flex items-center justify-between">
+                <div id="header-title">
+                    <a href="/" className="flex items-center gap-4 md:gap-6">
+                        <i className="bi bi-brightness-high-fill flex-center text-3xl"></i>
+                        <h1 className="font-semibold text-2xl hidden md:block">VoyageXplore</h1>
+                    </a>
+                </div>
+                <div id="header-navigation" className={"bg-white " + (navVisible ? "max-w-full" : "max-w-0") + " fixed top-0 right-0 z-20 lg:static overflow-hidden transition-all duration-300"}>
+                    <div id="navigation-container" className="border-l border-neutral-400 lg:border-none">
+                        <div id="close-container" className="w-full flex lg:hidden justify-end">
+                            <button onClick={() => setNavVisible(false)} className="p-1 block lg:hidden">
+                                <i className="bi bi-x flex-center text-2xl"></i>
+                            </button>
+                        </div>
+                        <div id="navigation-wrapper" className="flex flex-col-reverse lg:flex-row justify-end algin-start text-left items-center gap-x-12 p-6 lg:p-0 h-screen lg:h-auto">
+                            <nav id="header-navbar" className="flex flex-col lg:flex-row gap-6 my-8 lg:my-0">
+                                <a href="/" className="px-2 py-1 rounded hover:bg-neutral-200 active:bg-neutral-300 duration-150">Inicio</a>
+                                <a href="/commerce" className="px-2 py-1 rounded hover:bg-neutral-200 active:bg-neutral-300 duration-150">Comercio</a>
+                                <a href="/about-us" className="px-2 py-1 rounded hover:bg-neutral-200 active:bg-neutral-300 duration-150">Acerca de nosotros</a>
+                            </nav>
+                            <div id="signup-container">
+                                <a href="/login" className="rounded-lg px-4 py-2 bg-voyage-500 hover:bg-voyage-600 active:bg-voyage-700 text-white duration-150">Inicia sesión</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
+                <button onClick={() => { setNavVisible(!navVisible) }} className="block lg:hidden">
+                    <i className="bi bi-list text-2xl flex-center"></i>
+                </button>
             </div>
         </header>
     );

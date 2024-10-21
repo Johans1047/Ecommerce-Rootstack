1d832e444d61b08f5ab5fedeccb3e13fb267e3c3
Diego Chan <xdiego.chanx@gmail.com>
2024-10-21 11:51:26 -0500

    minor changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,16 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./styles/globals.css";
 
-createRoot(document.getElementById("root")).render(
+const router = createBrowserRouter([
+    {
+        
+    }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router}/>
+  </StrictMode>
 );

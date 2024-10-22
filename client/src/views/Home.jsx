f476eae938d9d5484768360e1922a0a36a46566f
Jonathan Salazar <jonyalex1047@gmail.com>
2024-10-21 19:10:33 -0500

    Info Blocks Section

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,11 +1,13 @@
 import NavBar from "../components/Header";
 import Search from "../components/Search";
+import Block from "../components/Block";
 
 export default function Home() {
     return (
         <>
             <NavBar />
             <Search />
+            <Block />
         </>
     );
 }
